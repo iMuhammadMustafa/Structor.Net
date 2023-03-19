b60facddb111575aaf3ca7fd797aebd54280af47
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-03-19 16:54:58 +0200

    Add FeatureTemplate

---
+++ Features/FeatureTemplate/FeatureDbContextExtension.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Structor.Net.Core.DatabaseContext
+{
+    public partial class CoreDbContext : DbContext
+    {
+    }
+}
---
+++ Features/FeatureTemplate/FeatureServicesCollection.cs
@@ -0,0 +1,11 @@
+﻿namespace Structor.Net.Features.FeatureTemplate;
+
+public static class FeatureServicesCollection
+{
+
+    public static IServiceCollection AddFeatureServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
