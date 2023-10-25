479ebc69a8a431bded2e82dbf833f70e97805acb
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-10-24 20:02:14 -0500

    Add ConfigurationSettings Template to appsettings.Development.json
    These Secrets should be in secrets.json on development and in Environment Variables on production or staging
    
    - Update Serilog Settings
    
    - Add Static Class to hold Configuration String Path
    
    -

---
+++ Structor/Core/Configurations/ConfigurationNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Structor.Core.Configurations;
+
+public static class ConfigurationNames
+{
+    public static readonly string JWT = "Auth:JWT";
+    public static readonly string OAUTH_CALLBACK = "Auth:OAuth:CallbackUrl";
+    public static readonly string OAUTH_OAuthUrl = "Auth:OAuth:OAuthUrl";
+    public static readonly string GITHUB = "Auth:OAuth:Github";
+    public static readonly string GOOGLE = "Auth:OAuth:Google";
+}
