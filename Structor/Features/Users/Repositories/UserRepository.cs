2c4267084cecdea23d0989f3bac2f8945b41e21b
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-10-24 20:08:29 -0500

    Update UserRepository.cs

--- Structor/Features/Users/Repositories/UserRepository.cs
+++ Structor/Features/Users/Repositories/UserRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Infrastructure.DatabaseContext;
+﻿using Infrastructure.DatabaseContext;
 using Structor.Features.Users.Entities;
 using Structor.Infrastructure.Repositories;
 
@@ -17,5 +12,25 @@ public class UserRepository : Repository<User, CoreDbContext>, IUserRepository
         _logger = logger;
     }
 
+
+    public async Task<bool> DoesUsernameExist(string username)
+    {
+        var user = await FindFirst(x=> x.Username == username);
+
+        return user != null;
+    }
+    public async Task<bool> DoesEmailExists(string email)
+    {
+        var user = await FindFirst(x => x.Email == email);
+
+        return user != null;
+    }
+
+    public async Task<User?> GetByEmailOrUsername(string loginBy)
+    {
+        return await FindFirst(x => x.Email.Equals(loginBy, StringComparison.OrdinalIgnoreCase) 
+                                 || string.IsNullOrWhiteSpace(x.Username) && x.Username!.Equals(loginBy, StringComparison.OrdinalIgnoreCase));
+
+    }
 
 }
